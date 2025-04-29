11bd7f091d034949d095d8d3de35ad254bb6f3a0
l-135 <luis.gomez.9267@gmail.com>
2025-04-28 18:32:17 -0700

    Db created/connected

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+
+const client = new Client({
+	user: 'postgres',
+	password: 'Thedoors6?',
+	host: 'localhost',
+	port: 5432,
+	database: '3680_final_project',
+});
+
+client
+.connect()
+.then(() => {
+  console.log('Connected to PostgreSQL database');
+})
+.catch((err) => {
+  console.error('Error connecting to PostgreSQL database', err);
+});
---
+++ backend/schema.sql
@@ -0,0 +1,53 @@
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "username" varchar(80) NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "comments" (
+  "id" serial PRIMARY KEY,
+  "title" varchar,
+  "body" text,
+  "user_id" integer NOT NULL,
+  "event_id" integer NOT NULL,
+  "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "events" (
+  "id" serial PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "location" varchar,
+  "date" date,
+  "description" text
+);
+
+CREATE TABLE "rsvps" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER,
+  "event_id" INTEGER
+);
+
+CREATE TABLE "messages" (
+  "id" SERIAL PRIMARY KEY,
+  "sender_id" integer,
+  "receiver_id" integer,
+  "body" text,
+  "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON COLUMN "comments"."body" IS 'Content of the post';
+
+COMMENT ON COLUMN "messages"."body" IS 'Content of the message';
+
+ALTER TABLE "comments" ADD CONSTRAINT "user_comments" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
+
+ALTER TABLE "messages" ADD FOREIGN KEY ("sender_id") REFERENCES "users" ("id");
+
+ALTER TABLE "messages" ADD FOREIGN KEY ("receiver_id") REFERENCES "users" ("id");
+
+ALTER TABLE "rsvps" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "rsvps" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
