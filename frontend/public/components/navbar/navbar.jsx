12017530d9a569fd55b8f1169210cc930a6cf0d8
HectorLopez2828 <hlopez40@csub.edu>
2025-05-06 10:00:27 -0700

    login/signup form

---
+++ frontend/public/components/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+export const navbar = () => {
+  return (
+     <div class="navbar">
+         <a class="navl" href="index.html">Search</a>
+         <a class="navl" href="index.html">Placeholder</a>
+         <a class="navl" href="index.html">Placeholder</a>
+    
+         <a class="navr" href="">Login</a>
+         <a class="navr" href="signup.html">Sign Up</a>
+
+         <br></br><br></br>
+          </div>
+
+  
+   )
+}  
+
