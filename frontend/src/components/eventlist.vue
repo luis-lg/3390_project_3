be4dd176a4cb254d584c05619dcbe045fdac2456
HectorLopez2828 <hlopez40@csub.edu>
2025-05-12 20:58:53 -0700

    eventlist screen style

--- frontend/src/components/eventlist.vue
+++ frontend/src/components/eventlist.vue
@@ -45,13 +45,29 @@ onMounted(fetchEvents)
 <style scoped>
 .event-list {
   padding: 1rem;
+  background-color: #fafafa;
 }
 .card {
-  padding: 0.75rem;
+  display: inline-flex;
+  padding: 2px 10px;
+  margin-right: 20px;
   margin-bottom: 0.5rem;
-  border: 1px solid #ddd;
+  border: 1px solid #d67c2e;
+  background-color: #f1c4b1;
+
 }
 .card h3 {
   margin: 0;
+  text-decoration: none;
+}
+
+p{
+  color: black;
+  text-decoration: none;
+}
+
+a{
+  color:black;
+  text-decoration: none;
 }
 </style>
