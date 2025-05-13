f5b7e29f489292a4bba8a35082407673affa97ab
HectorLopez2828 <hlopez40@csub.edu>
2025-05-12 17:26:20 -0700

    login screen style

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,11 +1,11 @@
 <template>
   <form @submit.prevent="doLogin">
     <h2>Log In</h2>
-    <div>
+    <div id="use">
       <label>Username</label>
       <input v-model="username" required />
     </div>
-    <div>
+    <div id="pass">
       <label>Password</label>
       <input v-model="password" type="password" required />
     </div>
@@ -37,3 +37,62 @@ const doLogin = async () => {
   }
 }
 </script>
+
+<style>
+h2{
+  font-weight: bold;
+}
+button{
+  display: inline-block;
+  margin-top: 10px;
+  width: 20%;
+  margin: auto;
+  background-color: #cfa391;
+  border-radius: 5px;
+}
+
+form{
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  text-align: center;
+  margin-top: 10px;
+  background-color: #fafafa;
+  height: 250px;
+  border-radius: 45px;
+}
+
+#use{
+  padding-top: 10px;
+  border-radius: 5px;
+}
+
+label{
+  font-weight: bold;
+  padding-right: 3px;
+}
+
+#use.label{
+  font-weight: bold;
+}
+
+#use.input{
+  padding-top: 20px;
+  margin-top: 10px;
+  width: 20%;
+}
+
+#pass{
+  padding-top: 10px;
+  padding-bottom: 10px; 
+}
+
+#pass.input{
+  margin-top: 10px;
+  width: 20%;
+}
+
+input{
+  border-radius: 5px;
+}
+</style>
